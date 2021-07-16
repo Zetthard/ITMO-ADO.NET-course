bbe8fd7925e7823bc2e74d63270c5b0f41298a9b
Semen Nikolaev <sr.nikolaev@gmail.com>
2021-07-16 21:53:01 +0300

    Revert "Revert "Ex 3 done""
    This reverts commit 2438c98deaff901d509ecc66f94e953b4831e13f.

---
+++ ConsoleADOTester/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+
+namespace ConsoleADOTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionStr = "Provider=SQLOLEDB.1;Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=ApressFinancial;Data Source=(local)";
+
+            // 1
+            using (SqlConnection connect = new SqlConnection(connectionStr))
+            {
+                connect.Open();
+
+                // 2
+                using (SqlCommand cmd = new SqlCommand("SELECT [ID],[Name],[Age] FROM [dbo].[Students] ORDER BY Name", connect))
+                {
+                    // 3
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine(reader["ID"] + " " + reader["Name"] + " " + reader["Age"]);
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+}
