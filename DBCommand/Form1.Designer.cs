84efc05f5b282742a126f19966236d0c57ef4381
Semen Nikolaev <sr.nikolaev@gmail.com>
2021-07-28 10:37:33 +0300

    Lab 3 Ex 4 done

--- DBCommand/Form1.Designer.cs
+++ DBCommand/Form1.Designer.cs
@@ -31,6 +31,8 @@ namespace DBCommand
         {
             this.sqlConnection1 = new System.Data.SqlClient.SqlConnection();
             this.sqlCommand1 = new System.Data.SqlClient.SqlCommand();
+            this.buttonGetData = new System.Windows.Forms.Button();
+            this.textBoxResults = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // sqlConnection1
@@ -39,14 +41,41 @@ namespace DBCommand
     " Security=True";
             this.sqlConnection1.FireInfoMessageEventOnUserErrors = false;
             // 
+            // sqlCommand1
+            // 
+            this.sqlCommand1.CommandText = "SELECT CustomerId, CustomerFirstName, CustomerLastName\r\nFROM  CustomerDetails.Cus" +
+    "tomers";
+            this.sqlCommand1.Connection = this.sqlConnection1;
+            // 
+            // buttonGetData
+            // 
+            this.buttonGetData.Location = new System.Drawing.Point(22, 22);
+            this.buttonGetData.Name = "buttonGetData";
+            this.buttonGetData.Size = new System.Drawing.Size(158, 73);
+            this.buttonGetData.TabIndex = 0;
+            this.buttonGetData.Text = "Get Data";
+            this.buttonGetData.UseVisualStyleBackColor = true;
+            this.buttonGetData.Click += new System.EventHandler(this.buttonGetData_Click);
+            // 
+            // textBoxResults
+            // 
+            this.textBoxResults.Location = new System.Drawing.Point(226, 21);
+            this.textBoxResults.Multiline = true;
+            this.textBoxResults.Name = "textBoxResults";
+            this.textBoxResults.Size = new System.Drawing.Size(514, 190);
+            this.textBoxResults.TabIndex = 1;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxResults);
+            this.Controls.Add(this.buttonGetData);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -54,6 +83,8 @@ namespace DBCommand
 
         private System.Data.SqlClient.SqlConnection sqlConnection1;
         private System.Data.SqlClient.SqlCommand sqlCommand1;
+        private System.Windows.Forms.Button buttonGetData;
+        private System.Windows.Forms.TextBox textBoxResults;
     }
 }
 
--- DBCommand/Form1.cs
+++ DBCommand/Form1.cs
@@ -18,5 +18,30 @@ namespace DBCommand
         {
             InitializeComponent();
         }
+
+        private void buttonGetData_Click(object sender, EventArgs e)
+        {
+            StringBuilder results = new StringBuilder();
+            sqlCommand1.CommandType = CommandType.Text;
+            sqlConnection1.Open();
+
+            SqlDataReader reader = sqlCommand1.ExecuteReader();
+            bool moreResults = false;
+            do
+            {
+                while (reader.Read())
+                {
+                    for (int i = 0; i < reader.FieldCount; i++)
+                        results.Append(reader[i].ToString() + "\t");
+                    results.Append(Environment.NewLine);
+                }
+                moreResults = reader.NextResult();
+            }
+            while (moreResults);
+
+            reader.Close();
+            sqlCommand1.Connection.Close();
+            textBoxResults.Text = results.ToString();
+        }
     }
 }
