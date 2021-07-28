d6ddb591023a5b10d88b48ee59e59ccd9139be49
Semen Nikolaev <sr.nikolaev@gmail.com>
2021-07-28 16:54:49 +0300

    Lab 3 and part 1 completed

--- DBCommand/Form1.Designer.cs
+++ DBCommand/Form1.Designer.cs
@@ -33,6 +33,18 @@ namespace DBCommand
             this.sqlCommand1 = new System.Data.SqlClient.SqlCommand();
             this.buttonGetData = new System.Windows.Forms.Button();
             this.textBoxResults = new System.Windows.Forms.TextBox();
+            this.buttonProcedure = new System.Windows.Forms.Button();
+            this.sqlCommandProcedure = new System.Data.SqlClient.SqlCommand();
+            this.buttonNewTable = new System.Windows.Forms.Button();
+            this.sqlCommandNewTable = new System.Data.SqlClient.SqlCommand();
+            this.buttonGetWithParam = new System.Windows.Forms.Button();
+            this.textBoxParam = new System.Windows.Forms.TextBox();
+            this.sqlCommandWithParam = new System.Data.SqlClient.SqlCommand();
+            this.buttonParamProcedure = new System.Windows.Forms.Button();
+            this.textBoxShareDecs = new System.Windows.Forms.TextBox();
+            this.sqlCommandParamProcedure = new System.Data.SqlClient.SqlCommand();
+            this.textBoxTickerId = new System.Windows.Forms.TextBox();
+            this.textBoxCurrPrice = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // sqlConnection1
@@ -49,9 +61,9 @@ namespace DBCommand
             // 
             // buttonGetData
             // 
-            this.buttonGetData.Location = new System.Drawing.Point(22, 22);
+            this.buttonGetData.Location = new System.Drawing.Point(22, 26);
             this.buttonGetData.Name = "buttonGetData";
-            this.buttonGetData.Size = new System.Drawing.Size(158, 73);
+            this.buttonGetData.Size = new System.Drawing.Size(158, 69);
             this.buttonGetData.TabIndex = 0;
             this.buttonGetData.Text = "Get Data";
             this.buttonGetData.UseVisualStyleBackColor = true;
@@ -65,11 +77,115 @@ namespace DBCommand
             this.textBoxResults.Size = new System.Drawing.Size(514, 190);
             this.textBoxResults.TabIndex = 1;
             // 
+            // buttonProcedure
+            // 
+            this.buttonProcedure.Location = new System.Drawing.Point(22, 117);
+            this.buttonProcedure.Name = "buttonProcedure";
+            this.buttonProcedure.Size = new System.Drawing.Size(158, 68);
+            this.buttonProcedure.TabIndex = 2;
+            this.buttonProcedure.Text = "Run Procedure";
+            this.buttonProcedure.UseVisualStyleBackColor = true;
+            this.buttonProcedure.Click += new System.EventHandler(this.buttonProcedure_Click);
+            // 
+            // sqlCommandProcedure
+            // 
+            this.sqlCommandProcedure.Connection = this.sqlConnection1;
+            // 
+            // buttonNewTable
+            // 
+            this.buttonNewTable.Location = new System.Drawing.Point(583, 234);
+            this.buttonNewTable.Name = "buttonNewTable";
+            this.buttonNewTable.Size = new System.Drawing.Size(157, 66);
+            this.buttonNewTable.TabIndex = 3;
+            this.buttonNewTable.Text = "New Table";
+            this.buttonNewTable.UseVisualStyleBackColor = true;
+            this.buttonNewTable.Click += new System.EventHandler(this.buttonNewTable_Click);
+            // 
+            // sqlCommandNewTable
+            // 
+            this.sqlCommandNewTable.Connection = this.sqlConnection1;
+            // 
+            // buttonGetWithParam
+            // 
+            this.buttonGetWithParam.Location = new System.Drawing.Point(226, 234);
+            this.buttonGetWithParam.Name = "buttonGetWithParam";
+            this.buttonGetWithParam.Size = new System.Drawing.Size(157, 43);
+            this.buttonGetWithParam.TabIndex = 4;
+            this.buttonGetWithParam.Text = "Get ...";
+            this.buttonGetWithParam.UseVisualStyleBackColor = true;
+            this.buttonGetWithParam.Click += new System.EventHandler(this.buttonGetWithParam_Click);
+            // 
+            // textBoxParam
+            // 
+            this.textBoxParam.Location = new System.Drawing.Point(22, 246);
+            this.textBoxParam.Name = "textBoxParam";
+            this.textBoxParam.Size = new System.Drawing.Size(158, 31);
+            this.textBoxParam.TabIndex = 5;
+            this.textBoxParam.Text = "1";
+            // 
+            // sqlCommandWithParam
+            // 
+            this.sqlCommandWithParam.Connection = this.sqlConnection1;
+            this.sqlCommandWithParam.Parameters.AddRange(new System.Data.SqlClient.SqlParameter[] {
+            new System.Data.SqlClient.SqlParameter("@titleId", System.Data.SqlDbType.Int, 0, System.Data.ParameterDirection.Input, false, ((byte)(0)), ((byte)(0)), "", System.Data.DataRowVersion.Current, "")});
+            // 
+            // buttonParamProcedure
+            // 
+            this.buttonParamProcedure.Location = new System.Drawing.Point(587, 365);
+            this.buttonParamProcedure.Name = "buttonParamProcedure";
+            this.buttonParamProcedure.Size = new System.Drawing.Size(153, 69);
+            this.buttonParamProcedure.TabIndex = 6;
+            this.buttonParamProcedure.Text = "Add Share";
+            this.buttonParamProcedure.UseVisualStyleBackColor = true;
+            this.buttonParamProcedure.Click += new System.EventHandler(this.buttonParamProcedure_Click);
+            // 
+            // textBoxShareDecs
+            // 
+            this.textBoxShareDecs.Location = new System.Drawing.Point(22, 365);
+            this.textBoxShareDecs.Name = "textBoxShareDecs";
+            this.textBoxShareDecs.Size = new System.Drawing.Size(158, 31);
+            this.textBoxShareDecs.TabIndex = 7;
+            this.textBoxShareDecs.Text = "Com Star Balt";
+            // 
+            // sqlCommandParamProcedure
+            // 
+            this.sqlCommandParamProcedure.CommandText = "ShareDetails.spu_AddShare";
+            this.sqlCommandParamProcedure.CommandType = System.Data.CommandType.StoredProcedure;
+            this.sqlCommandParamProcedure.Connection = this.sqlConnection1;
+            this.sqlCommandParamProcedure.Parameters.AddRange(new System.Data.SqlClient.SqlParameter[] {
+            new System.Data.SqlClient.SqlParameter("@decs", System.Data.SqlDbType.NVarChar),
+            new System.Data.SqlClient.SqlParameter("@tickerId", System.Data.SqlDbType.NVarChar),
+            new System.Data.SqlClient.SqlParameter("@currentPrice", System.Data.SqlDbType.Decimal)});
+            // 
+            // textBoxTickerId
+            // 
+            this.textBoxTickerId.Location = new System.Drawing.Point(213, 365);
+            this.textBoxTickerId.Name = "textBoxTickerId";
+            this.textBoxTickerId.Size = new System.Drawing.Size(158, 31);
+            this.textBoxTickerId.TabIndex = 8;
+            this.textBoxTickerId.Text = "CSB";
+            // 
+            // textBoxCurrPrice
+            // 
+            this.textBoxCurrPrice.Location = new System.Drawing.Point(400, 365);
+            this.textBoxCurrPrice.Name = "textBoxCurrPrice";
+            this.textBoxCurrPrice.Size = new System.Drawing.Size(158, 31);
+            this.textBoxCurrPrice.TabIndex = 9;
+            this.textBoxCurrPrice.Text = "3,40000";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxCurrPrice);
+            this.Controls.Add(this.textBoxTickerId);
+            this.Controls.Add(this.textBoxShareDecs);
+            this.Controls.Add(this.buttonParamProcedure);
+            this.Controls.Add(this.textBoxParam);
+            this.Controls.Add(this.buttonGetWithParam);
+            this.Controls.Add(this.buttonNewTable);
+            this.Controls.Add(this.buttonProcedure);
             this.Controls.Add(this.textBoxResults);
             this.Controls.Add(this.buttonGetData);
             this.Name = "Form1";
@@ -85,6 +201,18 @@ namespace DBCommand
         private System.Data.SqlClient.SqlCommand sqlCommand1;
         private System.Windows.Forms.Button buttonGetData;
         private System.Windows.Forms.TextBox textBoxResults;
+        private System.Windows.Forms.Button buttonProcedure;
+        private System.Data.SqlClient.SqlCommand sqlCommandProcedure;
+        private System.Windows.Forms.Button buttonNewTable;
+        private System.Data.SqlClient.SqlCommand sqlCommandNewTable;
+        private System.Windows.Forms.Button buttonGetWithParam;
+        private System.Windows.Forms.TextBox textBoxParam;
+        private System.Data.SqlClient.SqlCommand sqlCommandWithParam;
+        private System.Windows.Forms.Button buttonParamProcedure;
+        private System.Windows.Forms.TextBox textBoxShareDecs;
+        private System.Data.SqlClient.SqlCommand sqlCommandParamProcedure;
+        private System.Windows.Forms.TextBox textBoxTickerId;
+        private System.Windows.Forms.TextBox textBoxCurrPrice;
     }
 }
 
--- DBCommand/Form1.cs
+++ DBCommand/Form1.cs
@@ -43,5 +43,102 @@ namespace DBCommand
             sqlCommand1.Connection.Close();
             textBoxResults.Text = results.ToString();
         }
+
+        private void buttonProcedure_Click(object sender, EventArgs e)
+        {
+            StringBuilder results = new StringBuilder();
+            sqlCommandProcedure.CommandType = CommandType.StoredProcedure;
+            sqlCommandProcedure.CommandText = "CustomerDetails.sp_GetCustomersWithPositiveBal";
+            sqlConnection1.Open();
+
+            SqlDataReader reader = sqlCommandProcedure.ExecuteReader();
+            while (reader.Read())
+            {
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    results.Append(reader[i].ToString() + "\t");
+                }
+                results.Append(Environment.NewLine);
+            }
+            reader.Close();
+            sqlCommandProcedure.Connection.Close();
+            textBoxResults.Text = results.ToString();
+        }
+
+        private void buttonNewTable_Click(object sender, EventArgs e)
+        {
+            sqlCommandNewTable.CommandText = "CREATE TABLE [ShareDetails].[ShareTypes]" +
+                "([TypeId][bigint] IDENTITY(1, 1) NOT NULL, [TypeDecs] [nvarchar](50) NOT NULL)";
+            sqlConnection1.Open();
+            bool exCaught = false;
+            try
+            {
+                sqlCommandNewTable.ExecuteNonQuery();
+            }
+            catch (SqlException sqlex)
+            {
+                exCaught = true;
+                MessageBox.Show(sqlex.Message);
+            }
+            finally
+            {
+                sqlConnection1.Close();
+            }
+            if (!exCaught)
+                MessageBox.Show("Table has been created");
+        }
+
+        private void buttonGetWithParam_Click(object sender, EventArgs e)
+        {
+            StringBuilder results = new StringBuilder();
+            sqlCommandWithParam.CommandType = CommandType.Text;
+            sqlCommandWithParam.Parameters["@titleId"].Value = textBoxParam.Text;
+            sqlCommandWithParam.CommandText = "SELECT CustomerId, CustomerFirstName, CustomerLastName " +
+                "FROM CustomerDetails.Customers WHERE CustomerTitleId = @titleId";
+            sqlCommandWithParam.Connection.Open();
+
+            SqlDataReader reader = sqlCommandWithParam.ExecuteReader();
+            bool moreResults = false;
+            do
+            {
+                while (reader.Read())
+                {
+                    for (int i = 0; i < reader.FieldCount; i++)
+                        results.Append(reader[i].ToString() + "\t");
+                    results.Append(Environment.NewLine);
+                }
+                moreResults = reader.NextResult();
+            }
+            while (moreResults);
+
+            reader.Close();
+            sqlCommand1.Connection.Close();
+            textBoxResults.Text = results.ToString();
+        }
+
+        private void buttonParamProcedure_Click(object sender, EventArgs e)
+        {
+            sqlCommandParamProcedure.CommandType = CommandType.StoredProcedure;
+            sqlCommandParamProcedure.Parameters["@decs"].Value = textBoxShareDecs.Text;
+            sqlCommandParamProcedure.Parameters["@tickerId"].Value = textBoxTickerId.Text;
+            sqlCommandParamProcedure.Parameters["@currentPrice"].Value = Convert.ToDecimal(textBoxCurrPrice.Text);
+            sqlCommandParamProcedure.Connection.Open();
+            bool exCaught = false;
+            try
+            {
+                sqlCommandParamProcedure.ExecuteNonQuery();
+            }
+            catch (SqlException sqlex)
+            {
+                exCaught = true;
+                MessageBox.Show(sqlex.Message);
+            }
+            finally
+            {
+                sqlCommandParamProcedure.Connection.Close();
+            }
+            if (!exCaught)
+                MessageBox.Show("New Share has been added");
+        }
     }
 }
