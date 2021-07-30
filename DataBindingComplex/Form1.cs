c088a8543a9446592310fd9171d93a42a68be5a9
Semen Nikolaev <sr.nikolaev@gmail.com>
2021-07-30 09:49:20 +0300

    Lab 5 Ex 3 done

---
+++ DataBindingComplex/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataBindingComplex
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonBindGrid_Click(object sender, EventArgs e)
+        {
+            financialProductsTableAdapter1.Fill(apressFinancialDataSet1.FinancialProducts);
+            BindingSource productsBindingSource = new BindingSource(apressFinancialDataSet1, "FinancialProducts");
+            dataGridProducts.DataSource = productsBindingSource;
+            bindingNavigator1.BindingSource = productsBindingSource;
+        }
+    }
+}
