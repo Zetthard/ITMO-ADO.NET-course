3994ffd74149bd7d5189c2d8f2a650313f90f912
Semen Nikolaev <sr.nikolaev@gmail.com>
2021-07-30 09:37:11 +0300

    Lab 5 Ex 2 done

---
+++ DataBindingSilmple/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace DataBindingSilmple
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.apressFinancialDataSet1 = new DataBindingSilmple.ApressFinancialDataSet();
+            this.customersProductsTableAdapter1 = new DataBindingSilmple.ApressFinancialDataSetTableAdapters.CustomersProductsTableAdapter();
+            this.textBoxProductId = new System.Windows.Forms.TextBox();
+            this.textBoxProductName = new System.Windows.Forms.TextBox();
+            this.buttonPrevious = new System.Windows.Forms.Button();
+            this.buttonNext = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.apressFinancialDataSet1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // apressFinancialDataSet1
+            // 
+            this.apressFinancialDataSet1.DataSetName = "ApressFinancialDataSet";
+            this.apressFinancialDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // customersProductsTableAdapter1
+            // 
+            this.customersProductsTableAdapter1.ClearBeforeFill = true;
+            // 
+            // textBoxProductId
+            // 
+            this.textBoxProductId.Location = new System.Drawing.Point(20, 16);
+            this.textBoxProductId.Name = "textBoxProductId";
+            this.textBoxProductId.Size = new System.Drawing.Size(116, 31);
+            this.textBoxProductId.TabIndex = 0;
+            // 
+            // textBoxProductName
+            // 
+            this.textBoxProductName.Location = new System.Drawing.Point(175, 16);
+            this.textBoxProductName.Name = "textBoxProductName";
+            this.textBoxProductName.Size = new System.Drawing.Size(166, 31);
+            this.textBoxProductName.TabIndex = 1;
+            // 
+            // buttonPrevious
+            // 
+            this.buttonPrevious.Location = new System.Drawing.Point(20, 81);
+            this.buttonPrevious.Name = "buttonPrevious";
+            this.buttonPrevious.Size = new System.Drawing.Size(131, 48);
+            this.buttonPrevious.TabIndex = 2;
+            this.buttonPrevious.Text = "Previous";
+            this.buttonPrevious.UseVisualStyleBackColor = true;
+            this.buttonPrevious.Click += new System.EventHandler(this.buttonPrevious_Click);
+            // 
+            // buttonNext
+            // 
+            this.buttonNext.Location = new System.Drawing.Point(193, 81);
+            this.buttonNext.Name = "buttonNext";
+            this.buttonNext.Size = new System.Drawing.Size(131, 48);
+            this.buttonNext.TabIndex = 3;
+            this.buttonNext.Text = "Next";
+            this.buttonNext.UseVisualStyleBackColor = true;
+            this.buttonNext.Click += new System.EventHandler(this.buttonNext_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonNext);
+            this.Controls.Add(this.buttonPrevious);
+            this.Controls.Add(this.textBoxProductName);
+            this.Controls.Add(this.textBoxProductId);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.apressFinancialDataSet1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ApressFinancialDataSet apressFinancialDataSet1;
+        private ApressFinancialDataSetTableAdapters.CustomersProductsTableAdapter customersProductsTableAdapter1;
+        private System.Windows.Forms.TextBox textBoxProductId;
+        private System.Windows.Forms.TextBox textBoxProductName;
+        private System.Windows.Forms.Button buttonPrevious;
+        private System.Windows.Forms.Button buttonNext;
+    }
+}
+
---
+++ DataBindingSilmple/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataBindingSilmple
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private BindingSource productsBingingSource;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            customersProductsTableAdapter1.Fill(apressFinancialDataSet1.CustomersProducts);
+            productsBingingSource = new BindingSource(apressFinancialDataSet1, "CustomersProducts");
+
+            textBoxProductId.DataBindings.Add("Text", productsBingingSource, "CustomerFinancialProductId");
+            textBoxProductName.DataBindings.Add("Text", productsBingingSource, "AmountToCollect");
+        }
+
+        private void buttonPrevious_Click(object sender, EventArgs e)
+        {
+            productsBingingSource.MovePrevious();
+        }
+
+        private void buttonNext_Click(object sender, EventArgs e)
+        {
+            productsBingingSource.MoveNext();
+        }
+    }
+}
