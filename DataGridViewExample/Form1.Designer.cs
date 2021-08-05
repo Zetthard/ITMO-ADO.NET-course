8dc9cb76ac67ae909cfc4b84d9b4c0a22efee9db
Semen Nikolaev <sr.nikolaev@gmail.com>
2021-08-05 09:39:19 +0300

    Lab 5 Ex 4 done (without drag and drop); Lab 5 completed

---
+++ DataGridViewExample/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿
+namespace DataGridViewExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.buttonAddColumn = new System.Windows.Forms.Button();
+            this.buttonDeleteColumn = new System.Windows.Forms.Button();
+            this.buttonGetCell = new System.Windows.Forms.Button();
+            this.buttonApplyStyle = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.courseIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.titleDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.creditsDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.departmentIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.courseBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.schoolDataSet = new DataGridViewExample.SchoolDataSet();
+            this.courseTableAdapter = new DataGridViewExample.SchoolDataSetTableAdapters.CourseTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.courseBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.schoolDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.courseIDDataGridViewTextBoxColumn,
+            this.titleDataGridViewTextBoxColumn,
+            this.creditsDataGridViewTextBoxColumn,
+            this.departmentIDDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.courseBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(1, -1);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 82;
+            this.dataGridView1.RowTemplate.Height = 33;
+            this.dataGridView1.Size = new System.Drawing.Size(884, 524);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellValidating += new System.Windows.Forms.DataGridViewCellValidatingEventHandler(this.dataGridView1_CellValidating);
+            // 
+            // buttonAddColumn
+            // 
+            this.buttonAddColumn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonAddColumn.Location = new System.Drawing.Point(28, 547);
+            this.buttonAddColumn.Name = "buttonAddColumn";
+            this.buttonAddColumn.Size = new System.Drawing.Size(138, 48);
+            this.buttonAddColumn.TabIndex = 1;
+            this.buttonAddColumn.Text = "Add column";
+            this.buttonAddColumn.UseVisualStyleBackColor = true;
+            this.buttonAddColumn.Click += new System.EventHandler(this.buttonAddColumn_Click);
+            // 
+            // buttonDeleteColumn
+            // 
+            this.buttonDeleteColumn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonDeleteColumn.Location = new System.Drawing.Point(200, 547);
+            this.buttonDeleteColumn.Name = "buttonDeleteColumn";
+            this.buttonDeleteColumn.Size = new System.Drawing.Size(172, 48);
+            this.buttonDeleteColumn.TabIndex = 2;
+            this.buttonDeleteColumn.Text = "Delete Column";
+            this.buttonDeleteColumn.UseVisualStyleBackColor = true;
+            this.buttonDeleteColumn.Click += new System.EventHandler(this.buttonDeleteColumn_Click);
+            // 
+            // buttonGetCell
+            // 
+            this.buttonGetCell.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonGetCell.Location = new System.Drawing.Point(28, 615);
+            this.buttonGetCell.Name = "buttonGetCell";
+            this.buttonGetCell.Size = new System.Drawing.Size(138, 48);
+            this.buttonGetCell.TabIndex = 3;
+            this.buttonGetCell.Text = "Get Cell";
+            this.buttonGetCell.UseVisualStyleBackColor = true;
+            this.buttonGetCell.Click += new System.EventHandler(this.buttonGetCell_Click);
+            // 
+            // buttonApplyStyle
+            // 
+            this.buttonApplyStyle.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonApplyStyle.Location = new System.Drawing.Point(694, 547);
+            this.buttonApplyStyle.Name = "buttonApplyStyle";
+            this.buttonApplyStyle.Size = new System.Drawing.Size(172, 48);
+            this.buttonApplyStyle.TabIndex = 4;
+            this.buttonApplyStyle.Text = "Apply Style";
+            this.buttonApplyStyle.UseVisualStyleBackColor = true;
+            this.buttonApplyStyle.Click += new System.EventHandler(this.buttonApplyStyle_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(195, 627);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            // 
+            // courseIDDataGridViewTextBoxColumn
+            // 
+            this.courseIDDataGridViewTextBoxColumn.DataPropertyName = "CourseID";
+            this.courseIDDataGridViewTextBoxColumn.HeaderText = "CourseID";
+            this.courseIDDataGridViewTextBoxColumn.MinimumWidth = 10;
+            this.courseIDDataGridViewTextBoxColumn.Name = "courseIDDataGridViewTextBoxColumn";
+            this.courseIDDataGridViewTextBoxColumn.Width = 200;
+            // 
+            // titleDataGridViewTextBoxColumn
+            // 
+            this.titleDataGridViewTextBoxColumn.DataPropertyName = "Title";
+            this.titleDataGridViewTextBoxColumn.HeaderText = "Title";
+            this.titleDataGridViewTextBoxColumn.MinimumWidth = 10;
+            this.titleDataGridViewTextBoxColumn.Name = "titleDataGridViewTextBoxColumn";
+            this.titleDataGridViewTextBoxColumn.Width = 200;
+            // 
+            // creditsDataGridViewTextBoxColumn
+            // 
+            this.creditsDataGridViewTextBoxColumn.DataPropertyName = "Credits";
+            this.creditsDataGridViewTextBoxColumn.HeaderText = "Credits";
+            this.creditsDataGridViewTextBoxColumn.MinimumWidth = 10;
+            this.creditsDataGridViewTextBoxColumn.Name = "creditsDataGridViewTextBoxColumn";
+            this.creditsDataGridViewTextBoxColumn.Width = 200;
+            // 
+            // departmentIDDataGridViewTextBoxColumn
+            // 
+            this.departmentIDDataGridViewTextBoxColumn.DataPropertyName = "DepartmentID";
+            this.departmentIDDataGridViewTextBoxColumn.HeaderText = "DepartmentID";
+            this.departmentIDDataGridViewTextBoxColumn.MinimumWidth = 10;
+            this.departmentIDDataGridViewTextBoxColumn.Name = "departmentIDDataGridViewTextBoxColumn";
+            this.departmentIDDataGridViewTextBoxColumn.Width = 200;
+            // 
+            // courseBindingSource
+            // 
+            this.courseBindingSource.DataMember = "Course";
+            this.courseBindingSource.DataSource = this.schoolDataSet;
+            // 
+            // schoolDataSet
+            // 
+            this.schoolDataSet.DataSetName = "SchoolDataSet";
+            this.schoolDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // courseTableAdapter
+            // 
+            this.courseTableAdapter.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(888, 675);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonApplyStyle);
+            this.Controls.Add(this.buttonGetCell);
+            this.Controls.Add(this.buttonDeleteColumn);
+            this.Controls.Add(this.buttonAddColumn);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.courseBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.schoolDataSet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private SchoolDataSet schoolDataSet;
+        private System.Windows.Forms.BindingSource courseBindingSource;
+        private SchoolDataSetTableAdapters.CourseTableAdapter courseTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn courseIDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn titleDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn creditsDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn departmentIDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Button buttonAddColumn;
+        private System.Windows.Forms.Button buttonDeleteColumn;
+        private System.Windows.Forms.Button buttonGetCell;
+        private System.Windows.Forms.Button buttonApplyStyle;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ DataGridViewExample/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataGridViewExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'schoolDataSet.Course' table. You can move, or remove it, as needed.
+            this.courseTableAdapter.Fill(this.schoolDataSet.Course);
+            DataColumn location = new DataColumn("Location");
+            location.Expression = "Credits + ',' + DepartmentId";
+            schoolDataSet.Course.Columns.Add(location);
+        }
+
+        private void buttonAddColumn_Click(object sender, EventArgs e)
+        {
+            DataGridViewTextBoxColumn locationColumn = new DataGridViewTextBoxColumn();
+            locationColumn.Name = "LocationColumn";
+            locationColumn.HeaderText = "Location";
+            locationColumn.DataPropertyName = "Location";
+            dataGridView1.Columns.Add(locationColumn);
+        }
+
+        private void buttonDeleteColumn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                dataGridView1.Columns.Remove("LocationColumn");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void buttonGetCell_Click(object sender, EventArgs e)
+        {
+            string currentCellInfo;
+            currentCellInfo = dataGridView1.CurrentCell.Value.ToString() + "; ";
+            currentCellInfo += "Column: " + dataGridView1.CurrentCell.OwningColumn.DataPropertyName + "; ";
+            currentCellInfo += "Column Index: " + dataGridView1.CurrentCell.ColumnIndex.ToString() + "; ";
+            currentCellInfo += "Row Index: " + dataGridView1.CurrentCell.RowIndex.ToString();
+            label1.Text = currentCellInfo;
+        }
+
+        private void dataGridView1_CellValidating(object sender, DataGridViewCellValidatingEventArgs e)
+        {
+            if (dataGridView1.Columns[e.ColumnIndex].DataPropertyName == "Title")
+            {
+                if (e.FormattedValue.ToString() == "")
+                {
+                    dataGridView1.Rows[e.RowIndex].ErrorText = "Title is a required field";
+                    e.Cancel = true;
+                }
+                else
+                    dataGridView1.Rows[e.RowIndex].ErrorText = "";
+            }
+        }
+
+        private void buttonApplyStyle_Click(object sender, EventArgs e)
+        {
+            dataGridView1.AlternatingRowsDefaultCellStyle.BackColor = Color.LightGray;
+        }
+    }
+}
