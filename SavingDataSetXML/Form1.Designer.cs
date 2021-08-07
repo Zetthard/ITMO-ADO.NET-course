55ab794b920d4286dd403d023abdcaea225592dc
Semen Nikolaev <sr.nikolaev@gmail.com>
2021-08-07 16:15:18 +0300

    Lab 6 Ex 1 done

---
+++ SavingDataSetXML/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿
+namespace SavingDataSetXML
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.sqlSelectCommand1 = new System.Data.SqlClient.SqlCommand();
+            this.sqlInsertCommand1 = new System.Data.SqlClient.SqlCommand();
+            this.sqlUpdateCommand1 = new System.Data.SqlClient.SqlCommand();
+            this.sqlDeleteCommand1 = new System.Data.SqlClient.SqlCommand();
+            this.PersonDataAdapter = new System.Data.SqlClient.SqlDataAdapter();
+            this.sqlConnection1 = new System.Data.SqlClient.SqlConnection();
+            this.sqlSelectCommand2 = new System.Data.SqlClient.SqlCommand();
+            this.sqlInsertCommand2 = new System.Data.SqlClient.SqlCommand();
+            this.sqlUpdateCommand2 = new System.Data.SqlClient.SqlCommand();
+            this.sqlDeleteCommand2 = new System.Data.SqlClient.SqlCommand();
+            this.CourseDataAdapter = new System.Data.SqlClient.SqlDataAdapter();
+            this.schoolDataSet1 = new SavingDataSetXML.SchoolDataSet();
+            this.customerGrid = new System.Windows.Forms.DataGridView();
+            this.buttonFill = new System.Windows.Forms.Button();
+            this.buttonSaveData = new System.Windows.Forms.Button();
+            this.buttonSaveSchema = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.schoolDataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customerGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // sqlSelectCommand1
+            // 
+            this.sqlSelectCommand1.CommandText = "SELECT * FROM Person";
+            this.sqlSelectCommand1.Connection = this.sqlConnection1;
+            // 
+            // sqlInsertCommand1
+            // 
+            this.sqlInsertCommand1.CommandText = resources.GetString("sqlInsertCommand1.CommandText");
+            this.sqlInsertCommand1.Connection = this.sqlConnection1;
+            this.sqlInsertCommand1.Parameters.AddRange(new System.Data.SqlClient.SqlParameter[] {
+            new System.Data.SqlClient.SqlParameter("@LastName", System.Data.SqlDbType.NVarChar, 0, "LastName"),
+            new System.Data.SqlClient.SqlParameter("@FirstName", System.Data.SqlDbType.NVarChar, 0, "FirstName"),
+            new System.Data.SqlClient.SqlParameter("@HireDate", System.Data.SqlDbType.DateTime, 0, "HireDate"),
+            new System.Data.SqlClient.SqlParameter("@EnrollmentDate", System.Data.SqlDbType.DateTime, 0, "EnrollmentDate"),
+            new System.Data.SqlClient.SqlParameter("@Discriminator", System.Data.SqlDbType.NVarChar, 0, "Discriminator")});
+            // 
+            // sqlUpdateCommand1
+            // 
+            this.sqlUpdateCommand1.CommandText = resources.GetString("sqlUpdateCommand1.CommandText");
+            this.sqlUpdateCommand1.Connection = this.sqlConnection1;
+            this.sqlUpdateCommand1.Parameters.AddRange(new System.Data.SqlClient.SqlParameter[] {
+            new System.Data.SqlClient.SqlParameter("@LastName", System.Data.SqlDbType.NVarChar, 0, "LastName"),
+            new System.Data.SqlClient.SqlParameter("@FirstName", System.Data.SqlDbType.NVarChar, 0, "FirstName"),
+            new System.Data.SqlClient.SqlParameter("@HireDate", System.Data.SqlDbType.DateTime, 0, "HireDate"),
+            new System.Data.SqlClient.SqlParameter("@EnrollmentDate", System.Data.SqlDbType.DateTime, 0, "EnrollmentDate"),
+            new System.Data.SqlClient.SqlParameter("@Discriminator", System.Data.SqlDbType.NVarChar, 0, "Discriminator"),
+            new System.Data.SqlClient.SqlParameter("@Original_PersonID", System.Data.SqlDbType.Int, 0, System.Data.ParameterDirection.Input, false, ((byte)(0)), ((byte)(0)), "PersonID", System.Data.DataRowVersion.Original, null),
+            new System.Data.SqlClient.SqlParameter("@Original_LastName", System.Data.SqlDbType.NVarChar, 0, System.Data.ParameterDirection.Input, false, ((byte)(0)), ((byte)(0)), "LastName", System.Data.DataRowVersion.Original, null),
+            new System.Data.SqlClient.SqlParameter("@Original_FirstName", System.Data.SqlDbType.NVarChar, 0, System.Data.ParameterDirection.Input, false, ((byte)(0)), ((byte)(0)), "FirstName", System.Data.DataRowVersion.Original, null),
+            new System.Data.SqlClient.SqlParameter("@IsNull_HireDate", System.Data.SqlDbType.Int, 0, System.Data.ParameterDirection.Input, ((byte)(0)), ((byte)(0)), "HireDate", System.Data.DataRowVersion.Original, true, null, "", "", ""),
+            new System.Data.SqlClient.SqlParameter("@Original_HireDate", System.Data.SqlDbType.DateTime, 0, System.Data.ParameterDirection.Input, false, ((byte)(0)), ((byte)(0)), "HireDate", System.Data.DataRowVersion.Original, null),
+            new System.Data.SqlClient.SqlParameter("@IsNull_EnrollmentDate", System.Data.SqlDbType.Int, 0, System.Data.ParameterDirection.Input, ((byte)(0)), ((byte)(0)), "EnrollmentDate", System.Data.DataRowVersion.Original, true, null, "", "", ""),
+            new System.Data.SqlClient.SqlParameter("@Original_EnrollmentDate", System.Data.SqlDbType.DateTime, 0, System.Data.ParameterDirection.Input, false, ((byte)(0)), ((byte)(0)), "EnrollmentDate", System.Data.DataRowVersion.Original, null),
+            new System.Data.SqlClient.SqlParameter("@Original_Discriminator", System.Data.SqlDbType.NVarChar, 0, System.Data.ParameterDirection.Input, false, ((byte)(0)), ((byte)(0)), "Discriminator", System.Data.DataRowVersion.Original, null),
+            new System.Data.SqlClient.SqlParameter("@PersonID", System.Data.SqlDbType.Int, 4, "PersonID")});
+            // 
+            // sqlDeleteCommand1
+            // 
+            this.sqlDeleteCommand1.CommandText = resources.GetString("sqlDeleteCommand1.CommandText");
+            this.sqlDeleteCommand1.Connection = this.sqlConnection1;
+            this.sqlDeleteCommand1.Parameters.AddRange(new System.Data.SqlClient.SqlParameter[] {
+            new System.Data.SqlClient.SqlParameter("@Original_PersonID", System.Data.SqlDbType.Int, 0, System.Data.ParameterDirection.Input, false, ((byte)(0)), ((byte)(0)), "PersonID", System.Data.DataRowVersion.Original, null),
+            new System.Data.SqlClient.SqlParameter("@Original_LastName", System.Data.SqlDbType.NVarChar, 0, System.Data.ParameterDirection.Input, false, ((byte)(0)), ((byte)(0)), "LastName", System.Data.DataRowVersion.Original, null),
+            new System.Data.SqlClient.SqlParameter("@Original_FirstName", System.Data.SqlDbType.NVarChar, 0, System.Data.ParameterDirection.Input, false, ((byte)(0)), ((byte)(0)), "FirstName", System.Data.DataRowVersion.Original, null),
+            new System.Data.SqlClient.SqlParameter("@IsNull_HireDate", System.Data.SqlDbType.Int, 0, System.Data.ParameterDirection.Input, ((byte)(0)), ((byte)(0)), "HireDate", System.Data.DataRowVersion.Original, true, null, "", "", ""),
+            new System.Data.SqlClient.SqlParameter("@Original_HireDate", System.Data.SqlDbType.DateTime, 0, System.Data.ParameterDirection.Input, false, ((byte)(0)), ((byte)(0)), "HireDate", System.Data.DataRowVersion.Original, null),
+            new System.Data.SqlClient.SqlParameter("@IsNull_EnrollmentDate", System.Data.SqlDbType.Int, 0, System.Data.ParameterDirection.Input, ((byte)(0)), ((byte)(0)), "EnrollmentDate", System.Data.DataRowVersion.Original, true, null, "", "", ""),
+            new System.Data.SqlClient.SqlParameter("@Original_EnrollmentDate", System.Data.SqlDbType.DateTime, 0, System.Data.ParameterDirection.Input, false, ((byte)(0)), ((byte)(0)), "EnrollmentDate", System.Data.DataRowVersion.Original, null),
+            new System.Data.SqlClient.SqlParameter("@Original_Discriminator", System.Data.SqlDbType.NVarChar, 0, System.Data.ParameterDirection.Input, false, ((byte)(0)), ((byte)(0)), "Discriminator", System.Data.DataRowVersion.Original, null)});
+            // 
+            // PersonDataAdapter
+            // 
+            this.PersonDataAdapter.DeleteCommand = this.sqlDeleteCommand1;
+            this.PersonDataAdapter.InsertCommand = this.sqlInsertCommand1;
+            this.PersonDataAdapter.SelectCommand = this.sqlSelectCommand1;
+            this.PersonDataAdapter.TableMappings.AddRange(new System.Data.Common.DataTableMapping[] {
+            new System.Data.Common.DataTableMapping("Table", "Person", new System.Data.Common.DataColumnMapping[] {
+                        new System.Data.Common.DataColumnMapping("PersonID", "PersonID"),
+                        new System.Data.Common.DataColumnMapping("LastName", "LastName"),
+                        new System.Data.Common.DataColumnMapping("FirstName", "FirstName"),
+                        new System.Data.Common.DataColumnMapping("HireDate", "HireDate"),
+                        new System.Data.Common.DataColumnMapping("EnrollmentDate", "EnrollmentDate"),
+                        new System.Data.Common.DataColumnMapping("Discriminator", "Discriminator")})});
+            this.PersonDataAdapter.UpdateCommand = this.sqlUpdateCommand1;
+            // 
+            // sqlConnection1
+            // 
+            this.sqlConnection1.ConnectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=School;Integrated Security=Tru" +
+    "e";
+            this.sqlConnection1.FireInfoMessageEventOnUserErrors = false;
+            // 
+            // sqlSelectCommand2
+            // 
+            this.sqlSelectCommand2.CommandText = "SELECT * FROM Course";
+            this.sqlSelectCommand2.Connection = this.sqlConnection1;
+            // 
+            // sqlInsertCommand2
+            // 
+            this.sqlInsertCommand2.CommandText = resources.GetString("sqlInsertCommand2.CommandText");
+            this.sqlInsertCommand2.Connection = this.sqlConnection1;
+            this.sqlInsertCommand2.Parameters.AddRange(new System.Data.SqlClient.SqlParameter[] {
+            new System.Data.SqlClient.SqlParameter("@CourseID", System.Data.SqlDbType.Int, 0, "CourseID"),
+            new System.Data.SqlClient.SqlParameter("@Title", System.Data.SqlDbType.NVarChar, 0, "Title"),
+            new System.Data.SqlClient.SqlParameter("@Credits", System.Data.SqlDbType.Int, 0, "Credits"),
+            new System.Data.SqlClient.SqlParameter("@DepartmentID", System.Data.SqlDbType.Int, 0, "DepartmentID")});
+            // 
+            // sqlUpdateCommand2
+            // 
+            this.sqlUpdateCommand2.CommandText = resources.GetString("sqlUpdateCommand2.CommandText");
+            this.sqlUpdateCommand2.Connection = this.sqlConnection1;
+            this.sqlUpdateCommand2.Parameters.AddRange(new System.Data.SqlClient.SqlParameter[] {
+            new System.Data.SqlClient.SqlParameter("@CourseID", System.Data.SqlDbType.Int, 0, "CourseID"),
+            new System.Data.SqlClient.SqlParameter("@Title", System.Data.SqlDbType.NVarChar, 0, "Title"),
+            new System.Data.SqlClient.SqlParameter("@Credits", System.Data.SqlDbType.Int, 0, "Credits"),
+            new System.Data.SqlClient.SqlParameter("@DepartmentID", System.Data.SqlDbType.Int, 0, "DepartmentID"),
+            new System.Data.SqlClient.SqlParameter("@Original_CourseID", System.Data.SqlDbType.Int, 0, System.Data.ParameterDirection.Input, false, ((byte)(0)), ((byte)(0)), "CourseID", System.Data.DataRowVersion.Original, null),
+            new System.Data.SqlClient.SqlParameter("@Original_Title", System.Data.SqlDbType.NVarChar, 0, System.Data.ParameterDirection.Input, false, ((byte)(0)), ((byte)(0)), "Title", System.Data.DataRowVersion.Original, null),
+            new System.Data.SqlClient.SqlParameter("@Original_Credits", System.Data.SqlDbType.Int, 0, System.Data.ParameterDirection.Input, false, ((byte)(0)), ((byte)(0)), "Credits", System.Data.DataRowVersion.Original, null),
+            new System.Data.SqlClient.SqlParameter("@Original_DepartmentID", System.Data.SqlDbType.Int, 0, System.Data.ParameterDirection.Input, false, ((byte)(0)), ((byte)(0)), "DepartmentID", System.Data.DataRowVersion.Original, null)});
+            // 
+            // sqlDeleteCommand2
+            // 
+            this.sqlDeleteCommand2.CommandText = "DELETE FROM [Course] WHERE (([CourseID] = @Original_CourseID) AND ([Title] = @Ori" +
+    "ginal_Title) AND ([Credits] = @Original_Credits) AND ([DepartmentID] = @Original" +
+    "_DepartmentID))";
+            this.sqlDeleteCommand2.Connection = this.sqlConnection1;
+            this.sqlDeleteCommand2.Parameters.AddRange(new System.Data.SqlClient.SqlParameter[] {
+            new System.Data.SqlClient.SqlParameter("@Original_CourseID", System.Data.SqlDbType.Int, 0, System.Data.ParameterDirection.Input, false, ((byte)(0)), ((byte)(0)), "CourseID", System.Data.DataRowVersion.Original, null),
+            new System.Data.SqlClient.SqlParameter("@Original_Title", System.Data.SqlDbType.NVarChar, 0, System.Data.ParameterDirection.Input, false, ((byte)(0)), ((byte)(0)), "Title", System.Data.DataRowVersion.Original, null),
+            new System.Data.SqlClient.SqlParameter("@Original_Credits", System.Data.SqlDbType.Int, 0, System.Data.ParameterDirection.Input, false, ((byte)(0)), ((byte)(0)), "Credits", System.Data.DataRowVersion.Original, null),
+            new System.Data.SqlClient.SqlParameter("@Original_DepartmentID", System.Data.SqlDbType.Int, 0, System.Data.ParameterDirection.Input, false, ((byte)(0)), ((byte)(0)), "DepartmentID", System.Data.DataRowVersion.Original, null)});
+            // 
+            // CourseDataAdapter
+            // 
+            this.CourseDataAdapter.DeleteCommand = this.sqlDeleteCommand2;
+            this.CourseDataAdapter.InsertCommand = this.sqlInsertCommand2;
+            this.CourseDataAdapter.SelectCommand = this.sqlSelectCommand2;
+            this.CourseDataAdapter.TableMappings.AddRange(new System.Data.Common.DataTableMapping[] {
+            new System.Data.Common.DataTableMapping("Table", "Course", new System.Data.Common.DataColumnMapping[] {
+                        new System.Data.Common.DataColumnMapping("CourseID", "CourseID"),
+                        new System.Data.Common.DataColumnMapping("Title", "Title"),
+                        new System.Data.Common.DataColumnMapping("Credits", "Credits"),
+                        new System.Data.Common.DataColumnMapping("DepartmentID", "DepartmentID")})});
+            this.CourseDataAdapter.UpdateCommand = this.sqlUpdateCommand2;
+            // 
+            // schoolDataSet1
+            // 
+            this.schoolDataSet1.DataSetName = "SchoolDataSet";
+            this.schoolDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // customerGrid
+            // 
+            this.customerGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.customerGrid.Location = new System.Drawing.Point(12, 11);
+            this.customerGrid.Name = "customerGrid";
+            this.customerGrid.RowHeadersWidth = 82;
+            this.customerGrid.RowTemplate.Height = 33;
+            this.customerGrid.Size = new System.Drawing.Size(778, 256);
+            this.customerGrid.TabIndex = 0;
+            // 
+            // buttonFill
+            // 
+            this.buttonFill.Location = new System.Drawing.Point(12, 283);
+            this.buttonFill.Name = "buttonFill";
+            this.buttonFill.Size = new System.Drawing.Size(137, 65);
+            this.buttonFill.TabIndex = 1;
+            this.buttonFill.Text = "Fill";
+            this.buttonFill.UseVisualStyleBackColor = true;
+            this.buttonFill.Click += new System.EventHandler(this.buttonFill_Click);
+            // 
+            // buttonSaveData
+            // 
+            this.buttonSaveData.Location = new System.Drawing.Point(193, 283);
+            this.buttonSaveData.Name = "buttonSaveData";
+            this.buttonSaveData.Size = new System.Drawing.Size(137, 65);
+            this.buttonSaveData.TabIndex = 2;
+            this.buttonSaveData.Text = "Save XML Data";
+            this.buttonSaveData.UseVisualStyleBackColor = true;
+            this.buttonSaveData.Click += new System.EventHandler(this.buttonSaveData_Click);
+            // 
+            // buttonSaveSchema
+            // 
+            this.buttonSaveSchema.Location = new System.Drawing.Point(371, 283);
+            this.buttonSaveSchema.Name = "buttonSaveSchema";
+            this.buttonSaveSchema.Size = new System.Drawing.Size(137, 65);
+            this.buttonSaveSchema.TabIndex = 3;
+            this.buttonSaveSchema.Text = "Save XML Schema";
+            this.buttonSaveSchema.UseVisualStyleBackColor = true;
+            this.buttonSaveSchema.Click += new System.EventHandler(this.buttonSaveSchema_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonSaveSchema);
+            this.Controls.Add(this.buttonSaveData);
+            this.Controls.Add(this.buttonFill);
+            this.Controls.Add(this.customerGrid);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.schoolDataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customerGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Data.SqlClient.SqlCommand sqlSelectCommand1;
+        private System.Data.SqlClient.SqlConnection sqlConnection1;
+        private System.Data.SqlClient.SqlCommand sqlInsertCommand1;
+        private System.Data.SqlClient.SqlCommand sqlUpdateCommand1;
+        private System.Data.SqlClient.SqlCommand sqlDeleteCommand1;
+        private System.Data.SqlClient.SqlDataAdapter PersonDataAdapter;
+        private System.Data.SqlClient.SqlCommand sqlSelectCommand2;
+        private System.Data.SqlClient.SqlCommand sqlInsertCommand2;
+        private System.Data.SqlClient.SqlCommand sqlUpdateCommand2;
+        private System.Data.SqlClient.SqlCommand sqlDeleteCommand2;
+        private System.Data.SqlClient.SqlDataAdapter CourseDataAdapter;
+        private SchoolDataSet schoolDataSet1;
+        private System.Windows.Forms.DataGridView customerGrid;
+        private System.Windows.Forms.Button buttonFill;
+        private System.Windows.Forms.Button buttonSaveData;
+        private System.Windows.Forms.Button buttonSaveSchema;
+    }
+}
+
---
+++ SavingDataSetXML/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SavingDataSetXML
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonFill_Click(object sender, EventArgs e)
+        {
+            PersonDataAdapter.Fill(schoolDataSet1.Person);
+            CourseDataAdapter.Fill(schoolDataSet1.Course);
+            customerGrid.DataSource = schoolDataSet1.Person;
+        }
+
+        private void buttonSaveData_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                schoolDataSet1.WriteXml("SchoolDataSet.xml");
+                MessageBox.Show("XML file saved");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void buttonSaveSchema_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                schoolDataSet1.WriteXmlSchema("School.xsd");
+                MessageBox.Show("XML schema saved");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
